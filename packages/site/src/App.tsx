1c61384fac55a3c8b86b55b34166cbf630eacd58
artemioo <artemryb1@gmail.com>
2024-03-06 12:06:40 +0200

    add routes

--- packages/site/src/App.tsx
+++ packages/site/src/App.tsx
@@ -6,6 +6,14 @@ import { Footer, Header } from './components';
 import { GlobalStyle } from './config/theme';
 import { ToggleThemeContext } from './Root';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Chat}  from './pages/chats';
+import Index  from '../src/pages/index'
+import Main from './pages/main';
+import { CreateChat } from './pages/chats_create';
+import SendMessage from './pages/send_message';
+import ChatWithPartner from './pages/chat_with_partner';
+
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -26,7 +34,20 @@ export const App: FunctionComponent<AppProps> = ({ children }) => {
       <GlobalStyle />
       <Wrapper>
         <Header handleToggleClick={toggleTheme} />
-        {children}
+          <Router>
+            <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Index />} /> 
+            <Route path="/chats" element={<Chat />} />    
+            <Route path="/chats/:username" element={<ChatWithPartner />} /> 
+            <Route path="/chats/create" element={<CreateChat />} /> 
+            <Route path="/send-message/:username" element={<SendMessage />} /> 
+
+
+           {/*  {children} */}
+          </Routes>
+          </Router>
+
         <Footer />
       </Wrapper>
     </>
