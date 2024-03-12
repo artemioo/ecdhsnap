1983ed1537c34a56113376343fd3026bad3306fd
artemioo <artemryb1@gmail.com>
2024-03-12 11:29:56 +0200

    create GetPublicKey method

--- packages/snap/src/generate_keys.ts
+++ packages/snap/src/generate_keys.ts
@@ -14,3 +14,18 @@ export const GenerateNewPair = async (): Promise<string> => {
 
   return newPair.publicKey
 }
+
+
+export const GetPublicKey = async (): Promise<string> => {
+  const testPublickKey = await snap.request({
+    method: 'snap_getBip32PublicKey',
+    params: {
+      path: [ "m", "44'", "1'", "0'", "0", "0"],
+      curve: "secp256k1",
+      compressed: false,
+    }
+
+  })
+
+  return testPublickKey
+}
